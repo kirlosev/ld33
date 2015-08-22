447340e0ff1d1afc454e9c133c5ac97e8b5a7469
Kirill Losev <losukrat.contact@gmail.com>
2015-08-22 20:58:27 +0300

    refactoring done; jumping on faces of buildings works fine;

---
+++ LD33/Assets/scripts/MonsterBlood.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBlood : MonoBehaviour {
+    public Monster monster;
+
+    void FixedUpdate() {
+        var bloodParts = checkBlood();
+        for (var i = 0; i < bloodParts.Length; ++i) {
+            // TODO : increase specs
+            bloodParts[i].GetComponent<Blood>().destroy();
+        }
+    }
+
+    Collider2D[] checkBlood() {
+        return Physics2D.OverlapAreaAll(transform.position - monster.size, transform.position + monster.size, Game.instance.bloodLayer);
+    }
+}
--- LD33/Assets/scripts/MonsterMove.cs
+++ LD33/Assets/scripts/MonsterMove.cs
@@ -4,54 +4,61 @@ using System.Collections;
 public class MonsterMove : MonoBehaviour {
     public Monster monster;
 
-    public bool onGround = false;
-    public bool onSkyscraper = false;
+    public bool inAir = true;
     Vector3 velocity;
     Vector3 targetPoint;
-    WorldObject targetSkyscraper;
-    WorldObject currentSkyscraper;
+    Skyscraper targetSkyscraper;
+    Skyscraper currentSkyscraper;
     RaycastHit2D hit;
     float minTargetDistance;
     WorldObject sittingOnObject;
 
     void Update() {
-        if (monster.input.jump && (onGround || onSkyscraper)) {
+        if (monster.input.jump && !inAir) {
             var dir = (monster.input.clickPos - transform.position).normalized;
-
             velocity = dir * monster.jumpForce;
             targetPoint = monster.input.clickPos;
             minTargetDistance = Mathf.Infinity;
-            var col = Physics2D.OverlapCircle(monster.input.clickPos, 0.1f);
+            var col = Physics2D.OverlapCircle(monster.input.clickPos, 0.1f, Game.instance.skyscrapperLayer);
             if (col != null) {
-                targetSkyscraper = col.GetComponent<WorldObject>();
+                targetSkyscraper = col.GetComponent<Skyscraper>();
             }
-            onSkyscraper = false;
-            /*
-            if (Vector3.Dot(dir, hit.normal) > 0) {
-                velocity = dir * monster.jumpForce;
-                targetPoint = monster.input.clickPos;
-                minTargetDistance = Mathf.Infinity;
-                var col = Physics2D.OverlapCircle(monster.input.clickPos, 0.1f);
-                if (col != null) {
-                    targetSkyscraper = col.GetComponent<WorldObject>();
-                }
-                onSkyscraper = false;
-            }
-            */
+            else
+                targetSkyscraper = null;
+            inAir = true;
         }
-        
-        if (checkSkyscraper()) {
-            if (targetSkyscraper == null && currentSkyscraper != null) {
-                if (hit.collider.gameObject != currentSkyscraper.gameObject) {
+
+        if (checkGround()) {
+            if (inAir) {
+                velocity = Vector3.zero;
+                inAir = false;
+                transform.position = hit.point + hit.normal * monster.size.y;
+                var angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg - 90f;
+                transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            }
+        }
+        else if (checkWorldObject()) {
+            velocity = Vector3.zero;
+            inAir = false;
+            transform.position = (Vector3)hit.point
+                                 + Vector3.up * hit.normal.y * monster.size.y
+                                 + Vector3.right * hit.normal.x * monster.size.x;
+            var angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            sittingOnObject = hit.collider.GetComponent<WorldObject>();
+        }
+        else if (checkSkyscraper()) {
+            if (targetSkyscraper == null) {
+                if (hit.collider.GetComponent<Skyscraper>() != currentSkyscraper) {
                     velocity = Vector3.zero;
-                    currentSkyscraper = hit.collider.GetComponent<WorldObject>();
+                    currentSkyscraper = hit.collider.GetComponent<Skyscraper>();
                     targetSkyscraper = null;
-                    onSkyscraper = true;
+                    inAir = false;
                     var angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg - 90f;
                     transform.rotation = Quaternion.Euler(0f, 0f, angle);
                 }
             }
-            if (targetSkyscraper != null) {
+            else {
                 var targetDistance = (transform.position - targetPoint).magnitude;
                 if (targetDistance < minTargetDistance) {
                     minTargetDistance = targetDistance;
@@ -60,53 +67,28 @@ public class MonsterMove : MonoBehaviour {
                     velocity = Vector3.zero;
                     currentSkyscraper = targetSkyscraper;
                     targetSkyscraper = null;
-                    onSkyscraper = true;
+                    inAir = false;
                 }
             }
         }
-        /*
-        else
-            currentSkyscraper = null;
-            */
 
-        if (checkGround()) {
-            if (!onGround) {
-                velocity = Vector3.zero;
-                onGround = true;
-                //transform.position = hit.point + hit.normal * monster.size.y; // TODO : need better
-                var angle = Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg - 90f;
-                transform.rotation = Quaternion.Euler(0f, 0f, angle);
-                sittingOnObject = hit.collider.GetComponent<WorldObject>();
-            }
-        }
-        else onGround = false;
-        
-        if (onGround && sittingOnObject != null) {
+        if (sittingOnObject != null) {
             if (sittingOnObject.canThrow && monster.input.throwObject) {
                 var dir = (monster.input.clickPos - transform.position).normalized;
                 velocity = -dir * monster.jumpForce;
                 sittingOnObject.throwObject(dir, monster.throwForce);
-                onGround = false;
+                inAir = true;
             }
             else if (!sittingOnObject.canThrow && sittingOnObject.canBlood) {
                 sittingOnObject.damage(monster.damageValue);
             }
         }
 
-        
-
-        // TODO : move to a separate class
-        var bloodParts = checkBlood();
-        for (var i = 0; i < bloodParts.Length; ++i) {
-            // TODO : increase specs
-            bloodParts[i].GetComponent<Blood>().destroy();
-        }
-
         transform.position += velocity * Time.deltaTime;
-        if (!onGround && !onSkyscraper)
+        if (inAir) {
             velocity.y += Game.instance.gravity * Time.deltaTime;
-
-        if (velocity.magnitude > 0 && !onGround) {
+        }
+        if (velocity.magnitude > 0) {
             var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg - 90f;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
@@ -114,7 +96,13 @@ public class MonsterMove : MonoBehaviour {
 
     bool checkGround() {
         var dir = velocity.magnitude > 0 ? velocity.normalized : -1 * (Vector3)hit.normal;
-        hit = Physics2D.Raycast(transform.position, dir, monster.size.x, Game.instance.groundLayer | Game.instance.worldObjectLayer);
+        hit = Physics2D.Raycast(transform.position, dir, monster.size.x, Game.instance.groundLayer);
+        return hit;
+    }
+
+    bool checkWorldObject() {
+        var dir = velocity.magnitude > 0 ? velocity.normalized : -1 * (Vector3)hit.normal;
+        hit = Physics2D.Raycast(transform.position, dir, monster.size.x, Game.instance.worldObjectLayer);
         return hit;
     }
 
@@ -122,9 +110,5 @@ public class MonsterMove : MonoBehaviour {
         var dir = velocity.magnitude > 0 ? velocity.normalized : -1 * (Vector3)hit.normal;
         hit = Physics2D.Raycast(transform.position, dir, monster.size.x, Game.instance.groundLayer | Game.instance.skyscrapperLayer);
         return hit;
-    } 
-
-    Collider2D[] checkBlood() {
-        return Physics2D.OverlapAreaAll(transform.position - monster.size, transform.position + monster.size, Game.instance.bloodLayer);
     }
 }
